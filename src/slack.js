5507c7ae60c2fab990763deab94711a9eb49535d
Ariel Campoverde <ariel_mcm@outlook.com>
2018-02-08 14:14:14 -0500

    add error handler for slack request

--- src/slack.js
+++ src/slack.js
@@ -2,13 +2,19 @@ const webhookUrl = require('../../../tconf').WEBHOOK_URL;
 const axios = require('axios');
 
 export default class Slack {
-    static sendMessage (message) {
-        axios({
-            method: 'post',
-            url:    webhookUrl,
-            data:   {
-                attachments: [message]
-            }
-        });
+    static async sendMessage (message) {
+        try {
+            await axios({
+                method: 'post',
+                url:    webhookUrl,
+                data:   {
+                    attachments: [message]
+                }
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
+
     }
 }
