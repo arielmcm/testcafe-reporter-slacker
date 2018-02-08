0c9eb941531aeac564c299ad30dc0097dfcace1a
Ariel Campoverde <ariel_mcm@outlook.com>
2018-02-08 14:41:55 -0500

    implement success and failure messages

--- src/index.js
+++ src/index.js
@@ -25,27 +25,25 @@ export default function () {
         reportTaskDone (endTime, passed) {
             const durationMs = endTime - this.startTime;
             const durationStr = this.moment.duration(durationMs).format('h[h] mm[m] ss[s]');
-
             let text = `${this.agentsInfo}\n`;
 
-            Object.keys(this.failedTests).forEach(fixture => {
-                if (this.failedTests[fixture].length) {
-                    text = `${text}\n${fixture}\n`;
-                    this.failedTests[fixture].forEach(testName => {
-                        text = `${text}\n${testName}`;
-                    });
-                    text = `${text}\n`;
-                }
-            });
+            if (passed !== this.testCount) {
+                Object.keys(this.failedTests).forEach(fixture => {
+                    if (this.failedTests[fixture].length) {
+                        text = `${text}\n${fixture}\n`;
+                        this.failedTests[fixture].forEach(testName => {
+                            text = `${text}\n${testName}`;
+                        });
+                        text = `${text}\n`;
+                    }
+                });
+                text = `*Failed*\n${text}\n*${this.testCount - passed}/${this.testCount} tests failed (Duration: ${durationStr})*`;
+                Slack.sendErrorMessage(text);
+                return;
+            }
 
-            let footer = passed === this.testCount ?
-              `${this.testCount} passed` :
-              `${this.testCount - passed}/${this.testCount} failed`;
-
-            footer += ` (Duration: ${durationStr})`;
-            text = `${text}\n${footer}`;
-
-            Slack.sendMessage(text);
+            text = `*Succeeded*\n${text}\n*All tests passed (Duration: ${durationStr})*`;
+            Slack.sendSuccessMessage(text);
         }
     };
 }
--- src/slack.js
+++ src/slack.js
@@ -2,14 +2,20 @@ const webhookUrl = require('../../../tconf').WEBHOOK_URL;
 const axios = require('axios');
 
 export default class Slack {
-    static sendMessage (message) {
+    static sendSuccessMessage (message) {
+        Slack.sendMessage(message, 'good');
+    }
+    static sendErrorMessage (message) {
+        Slack.sendMessage(message, 'danger');
+    }
+    static sendMessage (message, type) {
         axios({
             method: 'post',
             url:    webhookUrl,
             data:   {
                 attachments: [{
                     text:  message,
-                    color: 'good'
+                    color: type
                 }]
             }
         }).catch(error => {
