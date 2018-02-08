217335287fddc7fc21f3baa5a10c37140e32dafe
Ariel Campoverde <ariel_mcm@outlook.com>
2018-02-08 14:48:27 -0500

    separe attachment from title

--- src/index.js
+++ src/index.js
@@ -1,49 +1,50 @@
 const Slack = require('./slack');
 
 export default function () {
-    return {
-        noColors: true,
+  return {
+    noColors: true,
 
-        reportTaskStart (startTime, userAgents, testCount) {
-            this.startTime = startTime;
-            this.testCount = testCount;
-            this.agentsInfo = `\`Running tests in: ${userAgents}\``;
-            this.failedTests = {};
-            this.currentFixture = '';
-        },
+    reportTaskStart (startTime, userAgents, testCount) {
+      this.startTime = startTime;
+      this.testCount = testCount;
+      this.agentsInfo = `\`Running tests in: ${userAgents}\``;
+      this.failedTests = {};
+      this.currentFixture = '';
+    },
 
-        reportFixtureStart (name) {
-            this.currentFixture = `*${name}*`;
-            this.failedTests[this.currentFixture] = [];
-        },
+    reportFixtureStart (name) {
+      this.currentFixture = `*${name}*`;
+      this.failedTests[this.currentFixture] = [];
+    },
 
-        reportTestDone (name, err) {
-            if (err.errs.length)
-                this.failedTests[this.currentFixture].push(`✗ ${name}`);
-        },
+    reportTestDone (name, err) {
+      if (err.errs.length) {
+        this.failedTests[this.currentFixture].push(`✗ ${name}`);
+      }
+    },
 
-        reportTaskDone (endTime, passed) {
-            const durationMs = endTime - this.startTime;
-            const durationStr = this.moment.duration(durationMs).format('h[h] mm[m] ss[s]');
-            let text = `${this.agentsInfo}\n`;
+    reportTaskDone (endTime, passed) {
+      const durationMs = endTime - this.startTime;
+      const durationStr = this.moment.duration(durationMs).format('h[h] mm[m] ss[s]');
+      let text = `${this.agentsInfo}\n`;
 
-            if (passed !== this.testCount) {
-                Object.keys(this.failedTests).forEach(fixture => {
-                    if (this.failedTests[fixture].length) {
-                        text = `${text}\n${fixture}\n`;
-                        this.failedTests[fixture].forEach(testName => {
-                            text = `${text}\n${testName}`;
-                        });
-                        text = `${text}\n`;
-                    }
-                });
-                text = `*Failed*\n${text}\n*${this.testCount - passed}/${this.testCount} tests failed (Duration: ${durationStr})*`;
-                Slack.sendErrorMessage(text);
-                return;
-            }
+      if (passed !== this.testCount) {
+        Object.keys(this.failedTests).forEach(fixture => {
+          if (this.failedTests[fixture].length) {
+            text = `${text}\n${fixture}\n`;
+            this.failedTests[fixture].forEach(testName => {
+              text = `${text}\n${testName}`;
+            });
+            text = `${text}\n`;
+          }
+        });
+        text = `${text}\n*${this.testCount - passed}/${this.testCount} tests failed (Duration: ${durationStr})*`;
+        Slack.sendErrorMessage(text);
+        return;
+      }
 
-            text = `*Succeeded*\n${text}\n*All tests passed (Duration: ${durationStr})*`;
-            Slack.sendSuccessMessage(text);
-        }
-    };
+      text = `${text}\n*All tests passed (Duration: ${durationStr})*`;
+      Slack.sendSuccessMessage(text);
+    }
+  };
 }
--- src/slack.js
+++ src/slack.js
@@ -2,24 +2,27 @@ const webhookUrl = require('../../../tconf').WEBHOOK_URL;
 const axios = require('axios');
 
 export default class Slack {
-    static sendSuccessMessage (message) {
-        Slack.sendMessage(message, 'good');
-    }
-    static sendErrorMessage (message) {
-        Slack.sendMessage(message, 'danger');
-    }
-    static sendMessage (message, type) {
-        axios({
-            method: 'post',
-            url:    webhookUrl,
-            data:   {
-                attachments: [{
-                    text:  message,
-                    color: type
-                }]
-            }
-        }).catch(error => {
-            console.log(error);
-        });
-    }
+  static sendSuccessMessage (message) {
+    Slack.sendMessage(message, 'good');
+  }
+
+  static sendErrorMessage (message) {
+    Slack.sendMessage(message, 'danger');
+  }
+
+  static sendMessage (message, type) {
+    axios({
+      method: 'post',
+      url: webhookUrl,
+      data: {
+        text: type === 'good' ? 'Succeeded' : 'Failed',
+        attachments: [{
+          text: message,
+          color: type
+        }]
+      }
+    }).catch(error => {
+      console.log(error);
+    });
+  }
 }
