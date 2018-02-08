835e360a90ee38c1d86f43a437d4f46a7ee3fe2e
Ariel Campoverde <ariel_mcm@outlook.com>
2018-02-08 14:10:01 -0500

    fix eslint issues

--- src/slack.js
+++ src/slack.js
@@ -5,10 +5,10 @@ export default class Slack {
     static sendMessage (message) {
         axios({
             method: 'post',
-            url: webhookUrl,
-            data: {
-                attachments: [message],
-            },
+            url:    webhookUrl,
+            data:   {
+                attachments: [message]
+            }
         });
     }
 }
