bb4d2f6cf1fd9a6a45e069e5e454d36305761f1e
Ariel Campoverde <ariel_mcm@outlook.com>
2018-02-09 11:20:14 -0500

    show console messages

--- src/index.js
+++ src/index.js
@@ -1,50 +1,117 @@
 const Slack = require('./slack');
+const shouldSendMessage = require('../../../tconf').SEND_TO_SLACK;
 
 export default function () {
   return {
-    noColors: true,
+    noColors: false,
 
     reportTaskStart (startTime, userAgents, testCount) {
       this.startTime = startTime;
       this.testCount = testCount;
-      this.agentsInfo = `\`Running tests in: ${userAgents}\``;
+      this.agentsInfo = `Running tests in: ${userAgents}`;
       this.failedTests = {};
       this.currentFixture = '';
+      this.write(this.chalk.blue(this.agentsInfo)).newline();
     },
 
     reportFixtureStart (name) {
       this.currentFixture = `*${name}*`;
       this.failedTests[this.currentFixture] = [];
+      this.newline().write(name).newline().newline();
     },
 
-    reportTestDone (name, err) {
-      if (err.errs.length) {
-        this.failedTests[this.currentFixture].push(`✗ ${name}`);
+    renderErrors (errs) {
+      this.setIndent(3).newline();
+
+      errs.forEach((err, idx) => {
+        const prefix = this.chalk.red(`${idx + 1}) `);
+
+        this
+          .write(this.formatError(err, prefix))
+          .newline()
+          .newline();
+      });
+    },
+
+    printTestSucceeded (test) {
+      this
+        .setIndent(1)
+        .write(this.chalk.green(`${this.symbols.ok} ${test}`))
+        .newline();
+    },
+
+    printTestFailed (test) {
+      this
+        .setIndent(1)
+        .write(this.chalk.bold.red(test))
+        .newline();
+    },
+
+    printTestSkipped (test) {
+      this
+        .setIndent(1)
+        .write(this.chalk.cyan(`- ${test}`))
+        .newline();
+    },
+
+    reportTestDone (name, testInfo) {
+      if (testInfo.errs.length) {
+        const errorTest = `${this.symbols.err} ${name}`;
+
+        this.failedTests[this.currentFixture].push(errorTest);
+        this.printTestFailed(errorTest);
+        this.renderErrors(testInfo.errs);
+        return;
+      }
+      if (testInfo.skipped) {
+        this.printTestSkipped(name);
+        return;
+      }
+      this.printTestSucceeded(name);
+    },
+
+    processSuccess (text, durationStr) {
+      const resultMessage = `All tests passed (Duration: ${durationStr})`;
+
+      text = `${text}\n*${resultMessage}*`;
+      this.newline().write(this.chalk.bold.green(resultMessage)).newline().newline();
+
+      if (shouldSendMessage) {
+        Slack.sendSuccessMessage(text);
+      }
+    },
+
+    processFailure (text, passed, duration) {
+      Object.keys(this.failedTests).forEach(fixture => {
+        if (this.failedTests[fixture].length) {
+          text = `${text}\n${fixture}\n`;
+          this.failedTests[fixture].forEach(testName => {
+            text = `${text}\n\t${testName}`;
+          });
+          text = `${text}\n`;
+        }
+      });
+      const resultMessage = `${this.testCount - passed}/${this.testCount} tests failed (Duration: ${duration})`;
+
+      text = `${text}\n*${resultMessage}*`;
+      this.setIndent(1).newline().write(this.chalk.bold.red(resultMessage)).newline().newline();
+
+      if (shouldSendMessage) {
+        Slack.sendErrorMessage(text);
       }
     },
 
     reportTaskDone (endTime, passed) {
       const durationMs = endTime - this.startTime;
-      const durationStr = this.moment.duration(durationMs).format('h[h] mm[m] ss[s]');
-      let text = `${this.agentsInfo}\n`;
+      const duration = this.moment.duration(durationMs).format('h[h] mm[m] ss[s]');
+      const text = `*${this.agentsInfo}*\n`;
 
-      if (passed !== this.testCount) {
-        Object.keys(this.failedTests).forEach(fixture => {
-          if (this.failedTests[fixture].length) {
-            text = `${text}\n${fixture}\n`;
-            this.failedTests[fixture].forEach(testName => {
-              text = `${text}\n\t${testName}`;
-            });
-            text = `${text}\n`;
-          }
-        });
-        text = `${text}\n*${this.testCount - passed}/${this.testCount} tests failed (Duration: ${durationStr})*`;
-        Slack.sendErrorMessage(text);
+      if (passed === this.testCount) {
+        this.processSuccess(text, duration);
         return;
       }
 
-      text = `${text}\n*All tests passed (Duration: ${durationStr})*`;
-      Slack.sendSuccessMessage(text);
+      this.processFailure(text, passed, duration);
     }
   };
 }
